8c2f4578af73062d269251296ec514f191a2d6fd
jn <riyan.husen@gmail.com>
2023-04-05 20:26:43 +0700

    update helper test customer for db

--- RealtaDB/HelperScriptGenerateData.sql
+++ RealtaDB/HelperScriptGenerateData.sql
@@ -1,4 +1,4 @@
-
+use Realta_DB
 select * from TestCustomer
 select * from TestCustomerLog
 select * from TestCopyCustomer
@@ -11,9 +11,9 @@ delete TestCustomerLog
 
 DECLARE @Counter INT 
 SET @Counter=1
-WHILE ( @Counter <= 10)
+WHILE ( @Counter <= 99)
 BEGIN
-    insert into TestCustomer(CustomerID,CustomerName,ContactName) values('Cust'+cast(@Counter as varchar(4)),'Name'+cast(@Counter as varchar(4)),'Contact'+cast(@Counter as varchar(4)))
+    insert into TestCustomer(CustomerID,CustomerName,ContactName) values('Cust'+right('0000'+cast(@Counter as varchar(4)),4),'Name'+cast(@Counter as varchar(4)),'Contact'+cast(@Counter as varchar(4)))
 
     SET @Counter  = @Counter  + 1
 END
